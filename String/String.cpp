70a247fa41356c25ae069de7750c2e0b5634d9f4
PatrickStar <1592810043@qq.com>
2022-10-02 22:59:39 +0800

    update compare and iostream

--- String/String.cpp
+++ String/String.cpp
@@ -53,7 +53,7 @@ char *String::getBuffer() const {
     return buffer;
 }
 
-void String::operator=(const String &right) {
+String &String::operator=(const String &right) {
     const unsigned rightLength = right.length();
     if (rightLength >= bufLen) {
         delete []buffer;
@@ -63,12 +63,134 @@ void String::operator=(const String &right) {
     }
     unsigned i = 0;
     for (; right.buffer[i] != '\0'; i ++) {
-
+        buffer[i] = right.buffer[i];
     }
+    buffer[i] = '\0';
+    return (*this);
+}
+
+String &String::operator=(const char *right) {
+    const unsigned rightLength = cStrLen(right);
+    if (rightLength >= bufLen) {
+        delete []buffer;
+        bufLen = 1 + rightLength;
+        buffer = new char[bufLen];
+        assert(buffer != nullptr);
+    }
+    unsigned i = 0;
+    for (; cStrLen(right) != '\0'; i ++) {
+        buffer[i] = right[i];
+    }
+    buffer[i] = '\0';
+    return (*this);
+}
+
+char nothing;
+char &String::operator[](unsigned index) {
+    if (index >= cStrLen(buffer)) {
+        nothing = '\0';
+        return nothing;
+    }
+    return buffer[index];
+}
+
+int String::compare(const String &right) {
+    char *p = buffer;
+    char *q = right.buffer;
+
+    for (; (*p != '\0') && (*p == *q); p ++, q ++);
+    return *p - *q;
+}
+
+String::operator const char *() const {
+    return buffer;
+}
+
+std::istream &String::getLine(std::istream &in) {
+    in.getline(buffer, bufLen, '\n');
+    return in;
 }
 
 unsigned cStrLen(const char *s) {
     unsigned len = 0;
     for(; s[len] != '\0'; len ++);
     return len;
+}
+
+std::istream &operator>>(std::istream &in, String &str) {
+    char inBuffer[1000];
+    if (in >> inBuffer) {
+        str = inBuffer;
+    } else {
+        str = " ";
+    }
+    return in;
+}
+
+std::ostream &operator<<(std::ostream &out, String &str) {
+    out << str.getBuffer();
+    return out;
+}
+
+int String::KMPMatch(const String &pattern) {
+    int *prefix = getPrefix(pattern.buffer, pattern.length());
+    int tar = 0, pos = 0;
+    while (tar < length()) {
+        if (buffer[tar] == pattern.buffer[pos]) {
+            tar ++;
+            pos ++;
+        } else if (pos != 0) {
+            pos = prefix[pos - 1];
+        } else {
+            tar ++;
+        }
+
+        if (pos == pattern.length()) {
+            return tar - pos;
+        }
+    }
+    return 0;
+}
+
+int *String::getPrefix(char *buffer_, int length) {
+    int *prefix = new int[length];
+    prefix[0] = 0;
+    int i = 1, now = 0;
+    while (i < length) {
+        if (buffer_[i] == buffer_[now]) {
+            now ++;
+            prefix[i] = now;
+            i ++;
+        } else if (now != 0) {
+            now = prefix[now - 1];
+        } else {
+            i ++;
+            prefix[i] = now;
+        }
+    }
+    return prefix;
+}
+
+bool String::operator<=(const String &right) {
+    return compare(right) <= 0;
+}
+
+bool String::operator<(const String &right) {
+    return compare(right) < 0;
+}
+
+bool String::operator>=(const String &right) {
+    return compare(right) >= 0;
+}
+
+bool String::operator>(const String &right) {
+    return compare(right) > 0;
+}
+
+bool String::operator==(const String &right) {
+    return compare(right) == 0;
+}
+
+bool String::operator!=(const String &right) {
+    return compare(right) != 0;
 }
--- String/String.h
+++ String/String.h
@@ -1,29 +1,46 @@
 #ifndef DATA_STRUCTURES_STRING_H
 #define DATA_STRUCTURES_STRING_H
-#include <assert.h>
+#include <cassert>
+#include <iostream>
 
 class String {
 public:
     String();
-    String(unsigned);
-    String(char);
-    String(const char *);
+    explicit String(unsigned);
+    explicit String(char);
+    explicit String(const char *);
     String(const String &);
     ~String();
 
-    void operator=(const String &);
+    String &operator=(const String &);
+    String &operator=(const char *);
+    char &operator[](unsigned);
+    explicit operator const char *() const;
+    bool operator<=(const String &right);
+    bool operator<(const String &right);
+    bool operator>=(const String &right);
+    bool operator>(const String &right);
+    bool operator==(const String &right);
+    bool operator!=(const String &right);
 
+
+    int compare(const String &);
     unsigned length() const;
     char *getBuffer() const;
+    int KMPMatch(const String &pattern);
 
+    std::istream &getLine(std::istream &);
 
 private:
     int len;
     unsigned bufLen;
     char *buffer;
 
+    int *getPrefix(char *buffer_, int length);
 };
 
 unsigned cStrLen(const char *);
+std::istream &operator>>(std::istream &, String &);
+std::ostream &operator<<(std::ostream &, String &);
 
 #endif //DATA_STRUCTURES_STRING_H
