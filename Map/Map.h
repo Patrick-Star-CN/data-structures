d7933a402f88a8e6bbbb13ba193fc992d7d44b53
PatrickStar <1592810043@qq.com>
2022-11-06 22:21:01 +0800

    update map

---
+++ Map/Map.h
@@ -0,0 +1,18 @@
+#ifndef DATA_STRUCTURES_MAP_H
+#define DATA_STRUCTURES_MAP_H
+
+template<typename T>
+class Map {
+private:
+    class Node {
+    private:
+        Node *left;
+        Node *right;
+        T data;
+    };
+
+public:
+
+};
+
+#endif //DATA_STRUCTURES_MAP_H
