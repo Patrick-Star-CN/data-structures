910d5197b7260e2c9cdfe00b38a02eb426d7cb13
PatrickStar <1592810043@qq.com>
2022-11-07 21:10:35 +0800

    update map

--- Map/Map.h
+++ Map/Map.h
@@ -1,15 +1,61 @@
 #ifndef DATA_STRUCTURES_MAP_H
 #define DATA_STRUCTURES_MAP_H
 
-template<typename T>
+template<typename T, typename U>
+class Pair {
+private:
+    T key;
+    U value;
+
+public:
+    explicit Pair(T k, U v): key(k), value(v) {}
+
+    T getKey() const;
+
+    void setKey(T key);
+
+    U getValue() const;
+
+    void setValue(U value);
+};
+
+template<typename T, typename U>
+T Pair<T, U>::getKey() const {
+    return key;
+}
+
+template<typename T, typename U>
+void Pair<T, U>::setKey(T key) {
+    Pair::key = key;
+}
+
+template<typename T, typename U>
+U Pair<T, U>::getValue() const {
+    return value;
+}
+
+template<typename T, typename U>
+void Pair<T, U>::setValue(U value) {
+    Pair::value = value;
+}
+
+template<typename T, typename U>
 class Map {
 private:
     class Node {
     private:
         Node *left;
         Node *right;
-        T data;
+        Pair<T, U> data;
     };
+
+    class Iterator {
+    private:
+        Node *ptr;
+
+    };
+
+    Node *root;
 
 public:
 
